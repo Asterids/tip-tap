40388c7d061233f760d2c9e8ab0eef73fda0782c
Ruth Hill <ruthhill.e@gmail.com>
2024-06-05 13:59:42 -0400

    get prettier involved

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import './App.css'
-import SampleText from './SampleText'
-import InputBox from './InputBox'
-import { useState } from 'react'
+import "./App.css";
+import SampleText from "./SampleText";
+import InputBox from "./InputBox";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [inputText, setInputText] = useState<string>('');
+  const [inputText, setInputText] = useState<string>("");
   const [isTestInProgress, setIsTestInProgress] = useState<boolean>(false);
+  const [testState, setTestState] = useState<
+    "waiting" | "running" | "completed"
+  >("waiting");
+  const [secondsRemaining, setSecondsRemaining] = useState<number>(30);
 
   const clearInput = () => {
-    setInputText('');
+    setInputText("");
     setIsTestInProgress(false);
   };
+
+  useEffect(() => {
+    if (testState === "running") {
+      setIsTestInProgress(true);
+    } else {
+      setIsTestInProgress(false);
+    }
+  }, [testState]);
 
   return (
     <div className="content">
@@ -18,15 +30,22 @@ function App() {
         <hr />
         <h1 className="title">tip tap</h1>
         <hr />
-        <p>Welcome to <i>tip tap</i>, a typing test!</p>
+        <p>
+          Welcome to <b>tip tap</b>, a typing test!
+        </p>
       </header>
 
       <div>
         <SampleText clearInput={clearInput} />
-        <InputBox inputText={inputText} isTestInProgress={isTestInProgress} handleKeypress={(e) => setInputText(e.target.value)} clearInput={clearInput} />
+        <InputBox
+          inputText={inputText}
+          isTestInProgress={isTestInProgress}
+          handleKeypress={(e) => setInputText(e.target.value)}
+          clearInput={clearInput}
+        />
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/SampleText.tsx
+++ src/SampleText.tsx
@@ -1,48 +1,80 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export type SelectableLanguages = 'english' | 'lorem' | 'french';
+export type SelectableLanguages = "english" | "lorem" | "french";
 
-export default function SampleText ({ clearInput }: any) {
-  const [selectedLang, setSelectedLang] = useState<SelectableLanguages>('english');
-  const [sampleText, setSampleText] = useState<string>('');
+export default function SampleText({ clearInput }: any) {
+  const [selectedLang, setSelectedLang] =
+    useState<SelectableLanguages>("english");
+  const [sampleText, setSampleText] = useState<string>("");
 
-  const generateSampleText = (lang: SelectableLanguages = 'english') => {
+  const generateSampleText = (lang: SelectableLanguages = "english") => {
     // dummy text for now
-    if (lang === 'lorem') {
-      setSampleText('Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.')
-    } else if (lang === 'french') {
-      setSampleText('Le chat est sur la table, le souris es sous de la table, et le fromage est triste. Il y a des choux fleurs dans la boîte la bas.')
+    if (lang === "lorem") {
+      setSampleText(
+        "Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet."
+      );
+    } else if (lang === "french") {
+      setSampleText(
+        "Le chat est sur la table, le souris es sous de la table, et le fromage est triste. Il y a des choux fleurs dans la boîte la bas."
+      );
     } else {
-      setSampleText('Sample text here. Sentences of words, the quick brown fox jumped over the lazy dog. Lorem ipsum dolor sit amet.')
+      setSampleText(
+        "Sample text here. Sentences of words, the quick brown fox jumped over the lazy dog. Lorem ipsum dolor sit amet."
+      );
     }
-  }
+  };
 
   const handleSelectTab = (lang: SelectableLanguages) => {
     setSelectedLang(lang);
     clearInput();
     generateSampleText(lang);
-  }
+  };
 
   const highlightIfSelected = (lang: string) => {
-    return selectedLang === lang ? 'selected' : '';
-  }
+    return selectedLang === lang ? "selected" : "";
+  };
 
   useEffect(() => {
     generateSampleText();
-  }, [])
+  }, []);
 
   return (
     <section className="sample-text-section" title="sample-text-section">
       <div className="lang-selection">
-        <a href="#tab" className={`${highlightIfSelected('english')}`} onClick={() => handleSelectTab('english')} data-value="english" role="tab" aria-controls="english">English</a>
-        <a href="#tab" className={`${highlightIfSelected('french')}`} onClick={() => handleSelectTab('french')} data-value="french" role="tab" aria-controls="french">French</a>
-        <a href="#tab" className={`${highlightIfSelected('lorem')}`} onClick={() => handleSelectTab('lorem')} data-value="lorem" role="tab" aria-controls="lorem">Lorem</a>
+        <a
+          href="#tab"
+          className={`${highlightIfSelected("english")}`}
+          onClick={() => handleSelectTab("english")}
+          data-value="english"
+          role="tab"
+          aria-controls="english"
+        >
+          English
+        </a>
+        <a
+          href="#tab"
+          className={`${highlightIfSelected("french")}`}
+          onClick={() => handleSelectTab("french")}
+          data-value="french"
+          role="tab"
+          aria-controls="french"
+        >
+          French
+        </a>
+        <a
+          href="#tab"
+          className={`${highlightIfSelected("lorem")}`}
+          onClick={() => handleSelectTab("lorem")}
+          data-value="lorem"
+          role="tab"
+          aria-controls="lorem"
+        >
+          Lorem
+        </a>
       </div>
       <div className="text-block">
-        <p>
-          {sampleText}
-        </p>
+        <p>{sampleText}</p>
       </div>
     </section>
-  )
-};
+  );
+}
